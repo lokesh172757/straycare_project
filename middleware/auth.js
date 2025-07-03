0f74210c8cfe065a78041529c0059803e1682193
Suhani Sharma <145221387+suhanijourneyongithub@users.noreply.github.com>
2025-07-03 22:00:00 +0530

    first commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
 // middleware/auth.js
 
-exports.isLoggedIn = (req, res, next) => {
-    if (!req.session.userId) {
+exports.isLoggedIn = (req, res, next) =>{
+    if (!req.session.userId){
         return res.redirect("/auth/login/local");
     }
     next();
 };
 
-exports.isLocalUser = (req, res, next) => {
+exports.isLocalUser = (req, res, next) =>  {
     if (req.session.userKind !== "LOCAL") {
         return res.status(403).send("Only public users can access this.");
     }
     next();
 };
 
-exports.isNGO = (req, res, next) => {
-    if (req.session.userKind !== "NGO") {
+exports.isNGO = (req, res, next) =>{
+    if (req.session.userKind !== "NGO"){
         return res.status(403).send("Only NGOs can access this.");
     }
     next();
